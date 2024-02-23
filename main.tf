6fb81ae265eba7c8089fdf17b4a54e2403614007
Betzabet Chaves <betzabet_chaves@hakkoda.io>
2024-02-23 15:44:39 -0600

    fix

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ resource "snowflake_grant_privileges_to_role" "database_grant" {
   provider   = snowflake.security_admin
   privileges = ["USAGE"]
   role_name  = snowflake_role.role.name
+
   on_account_object {
     object_type = "DATABASE"
     object_name = snowflake_database.db.name
